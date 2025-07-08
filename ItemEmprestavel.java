f0e993a8f79630b750d0af1011a2598e19f7f7d0
BMO-O <151739349+BMO-O@users.noreply.github.com>
2025-07-07 21:34:54 -0300

    Update ItemEmprestavel.java

--- ItemEmprestavel.java
+++ ItemEmprestavel.java
@@ -1,5 +1,11 @@
 import java.io.Serializable;
 
+/*
+Interface que define o contrato para itens que podem ser emprestados.
+Estende a interface Serializable para permitir que os objetos sejam salvos em arquivos.
+ */
+
 public interface ItemEmprestavel extends Serializable {
+    //Retorna a descrição textual do item emprestável.
     String getDesc();
 }
